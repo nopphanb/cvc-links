c72fbe19fc8be143d55cf477ffe3eb7d031ff814
nopphanb <41207240+nopphanb@users.noreply.github.com>
2022-01-07 07:32:17 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'head' : ['Prompt', 'sans-serif'],
+    },
+    container:{
+      center : true,
+      padding : '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
